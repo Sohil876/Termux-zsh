72545d26ebb1e31d18de33ec3fe38dcf015dca21
Sohil876 <sohil876@gmail.com>
2019-11-06 21:32:54 +0530

    Added ability to install selectable framework, info added in readme

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ My personal termux setup, a bit modernized terminal :)
 
 **What it does?:** 
 - Installs zsh and sets it as default shell.
-- Installs oh-my-zsh framework for plugins and themes.
+- Installs user selectable zsh framework from available options (1) zplugin, (2) oh-my-zsh, (3) Prezto for plugins and themes.
 - Installs Powerlevel9k theme and prepatched SourceCodePro font as default.
-- Added fonts and colors im ~/.termux/ directory with scripts to change them.
+- Added fonts and colors in ~/.termux/ directory with scripts to change them easily.
 - Installs syntax highlighter and autosuggestion plugins.
-- Disable unnecessary infos on right prompt and unnecessary userinfo on left prompt.
+- Disable unnecessary infos on right and left prompts.
 - Enable double line left prompt with diffrent icons for dir, git and vcs, beautified left command prompt a bit, enabled new line after prompt.
 - Replace termux's default welcome message with a better looking one.
 
--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #DIR="$(pwd)"
+
 # Install dependencies
-echo "Installing dependencies."
+echo "Installing dependencies:"
 apt update && apt install -y git zsh wget figlet
 
 # Create termux home directory if not exists
@@ -11,30 +12,58 @@ apt update && apt install -y git zsh wget figlet
 
 # Replacing termuxs boring welcome message with something good looking
 mv $PREFIX/etc/motd $PREFIX/etc/motd.bak
-#sed '/^export ZSH=$HOME*/a\figlet Termux\n(Setup by: Sohil876)' $HOME/.zshrc
+# message included in *rc files
 
-# Download and setup oh-my-zsh
-echo "Downloading and setting up oh-my-zsh."
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
-#cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
-
-# Copy .termux folder
-cp -r .termux/ $HOME/.termux
-chmod +x $HOME/.termux/fonts.sh $HOME/.termux/colors.sh
-
-# Powerlevel9k theme and sorce code pro powerline font
-echo "Installing powerlevel9k theme and source code pro powerline font."
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-#sed -i '/ZSH_THEME="robbyrussell"/c\ZSH_THEME="powerlevel9k/powerlevel9k"' $HOME/.zshrc
-wget https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf -O $HOME/.termux/font.ttf
-#sed '/^ZSH_THEME=.*/i POWERLEVEL9K_MODE="awesome-patched"' $HOME/.zshrc
-
-# Adding plugins
-echo "Setting up plugins."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
-#sed -i '/plugins=(git)/c\plugins=(git zsh-syntax-highlighting zsh-autosuggestions)' $HOME/.zshrc
-echo "Installed and enabled syntax highlighter and autosuggestion plugins."
+echo "Select installation:"
+echo "Enter 1 to select zplugin, 2 for oh-my-zsh and 3 for prezto"
+read -p "|=> " choice;
+if [[ $choice == 1 ]] ; then
+  echo "zplugin selected!"
+  # Install plugin dependencies
+  echo "Installing plugin required dependencies:"
+  apt update && apt install -y make file
+  # Copy .termux folder
+  cp -r .termux/ ~/.termux
+  chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
+  # Download and setup zplugin
+  echo "Downloading and setting up zlugin."
+  mkdir ~/.zplugin
+  git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+  mkdir ~/my_completions
+  # Copying over configured .zshrc file
+  cp zp.zshrc $HOME/.zshrc
+  touch ~/.zsh_history
+  chmod +rw ~/.zsh_history
+  echo "zplugin downloaded and configured!, to start the installation,"
+elif [[ $choice == 2 ]] ; then
+  echo "oh-my-zsh selected!"
+  # Copy .termux folder
+  cp -r .termux/ ~/.termux
+  chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
+  # Download and setup oh-my-zsh
+  echo "Downloading and setting up oh-my-zsh."
+  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+  # Powerlevel9k theme and source code pro powerline font
+  echo "Installing powerlevel9k theme with source code pro powerline font support."
+  git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+  #wget https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf -O $HOME/.termux/font.ttf
+  # Adding plugins
+  echo "Setting up plugins."
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+  echo "Installed and enabled syntax highlighter and autosuggestion plugins."
+  # Copying over configured .zshrc file
+  #cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+  cp omz.zshrc ~/.zshrc
+  echo "Finished installing oh-my-zsh!"
+elif [[ $choice == 3 ]]; then
+  echo "prezto selected!"
+  echo "Preztzo support to come in time :p"
+  exit 1;
+else
+  echo "Invalid choice!"
+  exit 1;
+fi
 
 # Remove the unnecessary userinfo on left prompt
 #sed '/^# alias ohmyzsh=*/a\prompt_context() {}' $HOME/.zshrc
@@ -43,7 +72,6 @@ echo "Installed and enabled syntax highlighter and autosuggestion plugins."
 echo "Setting up zsh as default."
 chsh -s zsh
 #mv $DIR/.files/colors.properties $HOME/.termux/colors.properties
-cp my.zshrc $HOME/.zshrc
 
 # Setup Complete
 termux-setup-storage
