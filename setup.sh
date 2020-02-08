b846a9f4cd52884c663ed40197f99a2a5ab6a896
Sohil876 <sohil876@gmail.com>
2020-02-09 04:00:35 +0530

    Finally added prezto! ,made some changes to repo structure

--- setup.sh
+++ setup.sh
@@ -15,32 +15,31 @@ mv $PREFIX/etc/motd $PREFIX/etc/motd.bak
 # message included in *rc files
 
 echo "Select installation:"
-echo "Best use OhMyZsh unless you want a very complex setup with zplugin."
-echo "Enter 1 to select Zplugin, 2 for Oh-My-Zsh and 3 for Prezto"
-read -p "|=> " choice;
+echo "Enter 1 to select Zinit (Zplugin), 2 for Oh-My-Zsh and 3 for Prezto"
+vared -p "|=> " -c choice;
 if [[ $choice == 1 ]] ; then
-  echo "Zplugin selected!"
+  echo "Zinit selected!"
   # Install plugin dependencies
-  echo "Installing plugin required dependencies:"
+  echo "Installing plugin dependencies:"
   apt update && apt install -y make file
   # Copy .termux folder
-  cp -r .termux/ ~/.termux
+  cp -fr Termux/ ~/.termux
   chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
-  # Download and setup zplugin
-  echo "Downloading and setting up Zplugin."
-  mkdir ~/.zplugin
-  git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+  # Download and setup Zinit
+  echo "Downloading and setting up Zinit:"
+  mkdir ~/.zinit
+  git clone --depth=1 https://github.com/zdharma/zinit.git ~/.zinit/bin
   mkdir ~/my_completions
   # Copying over configured .zshrc file
-  cp zp.zshrc $HOME/.zshrc
+  cp -f Zinit/zshrc ~/.zshrc
   touch ~/.zsh_history
   chmod +rwx ~/.zshrc
   chmod +rw ~/.zsh_history
-  echo "Zplugin downloaded and configured!, install will proceed on termux restart!"
+  echo "Zinit downloaded and configured!, install will proceed on termux restart!"
 elif [[ $choice == 2 ]] ; then
   echo "Oh-My-Zsh selected!"
   # Copy .termux folder
-  cp -r .termux/ ~/.termux
+  cp -fr Termux/ ~/.termux
   chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
   # Download and setup oh-my-zsh
   echo "Downloading and setting up Oh-My-Zsh."
@@ -51,18 +50,34 @@ elif [[ $choice == 2 ]] ; then
   #wget https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf -O $HOME/.termux/font.ttf
   # Adding plugins
   echo "Setting up plugins."
-  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
   echo "Installed and enabled syntax highlighter and autosuggestion plugins."
   # Copying over configured .zshrc file
   #cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-  cp omz.zshrc ~/.zshrc
+  cp -f OhMyZsh/zshrc ~/.zshrc
   chmod +rwx ~/.zshrc
   chmod +rw ~/.zsh_history
   echo "Finished installing Oh-My-Zsh!"
 elif [[ $choice == 3 ]]; then
   echo "Prezto selected!"
-  echo "Preztzo support to come in time, if i feel like using it :p"
+  # Copy .termux folder
+  cp -fr Termux/ ~/.termux
+  chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
+  # Download and setup Prezto
+  echo "Downloading and setting up Prezto."
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  # Create rc files
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+  # Copying over configured files
+  cp -f Prezto/zpreztorc ~/.zpreztorc
+  cp -f Prezto/zshrc ~/.zshrc
+  chmod +rwx ~/.zshrc
+  chmod +rw ~/.zsh_history
+  echo "Finished installing Prezto!"
   exit 1;
 else
   echo "Invalid choice!"
