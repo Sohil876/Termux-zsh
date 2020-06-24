8e0f495a5e06e3b5daf2cbd57103e642775faae0
Sohil876 <sohil876@gmail.com>
2020-06-24 21:42:54 +0530

    Dropped other frameworks, prezto is only supported, fixed paths and readme

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ My personal termux setup, a bit modernized terminal :)
 
 **What it does?:** 
 - Installs zsh and sets it as default shell.
-- Installs user selectable zsh framework from available options (1) zplugin, (2) oh-my-zsh, (3) Prezto for plugins and themes.
-- Installs Powerlevel9k theme and prepatched SourceCodePro font as default.
+- Installs and sets precustomized Prezto as default 
+- ~~Installs user selectable zsh framework from available options (1) zplugin, (2) oh-my-zsh, (3) Prezto for plugins and themes.~~ (Dropped)
+- Installs Powerlevel10k theme and prepatched SourceCodePro font as default.
 - Added fonts and colors in ~/.termux/ directory with scripts to change them easily.
 - Installs syntax highlighter and autosuggestion plugins.
 - Disable unnecessary infos on right and left prompts.
--- setup.sh
+++ setup.sh
@@ -15,51 +15,10 @@ mv $PREFIX/etc/motd $PREFIX/etc/motd.bak
 # message included in *rc files
 
 echo "Select installation:"
-echo "Enter 1 to select Zinit (Zplugin), 2 for Oh-My-Zsh and 3 for Prezto"
+echo "N.O.T.E:= Dropped other plugin managers in favour of Prezto"
+echo "Enter 1 to install and set Prezto as default."
 vared -p "|=> " -c choice;
-if [[ $choice == 1 ]] ; then
-  echo "Zinit selected!"
-  # Install plugin dependencies
-  echo "Installing plugin dependencies:"
-  apt update && apt install -y make file
-  # Copy .termux folder
-  cp -fr Termux/ ~/.termux
-  chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
-  # Download and setup Zinit
-  echo "Downloading and setting up Zinit:"
-  mkdir ~/.zinit
-  git clone --depth=1 https://github.com/zdharma/zinit.git ~/.zinit/bin
-  mkdir ~/my_completions
-  # Copying over configured .zshrc file
-  cp -f Zinit/zshrc ~/.zshrc
-  touch ~/.zsh_history
-  chmod +rwx ~/.zshrc
-  chmod +rw ~/.zsh_history
-  echo "Zinit downloaded and configured!, install will proceed on termux restart!"
-elif [[ $choice == 2 ]] ; then
-  echo "Oh-My-Zsh selected!"
-  # Copy .termux folder
-  cp -fr Termux/ ~/.termux
-  chmod +x ~/.termux/fonts.sh ~/.termux/colors.sh
-  # Download and setup oh-my-zsh
-  echo "Downloading and setting up Oh-My-Zsh."
-  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-  # Powerlevel10k theme and source code pro powerline font
-  echo "Installing powerlevel10k theme with source code pro powerline font support."
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-  #wget https://github.com/gabrielelana/awesome-terminal-fonts/raw/patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf -O $HOME/.termux/font.ttf
-  # Adding plugins
-  echo "Setting up plugins."
-  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-  echo "Installed and enabled syntax highlighter and autosuggestion plugins."
-  # Copying over configured .zshrc file
-  #cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-  cp -f OhMyZsh/zshrc ~/.zshrc
-  chmod +rwx ~/.zshrc
-  chmod +rw ~/.zsh_history
-  echo "Finished installing Oh-My-Zsh!"
-elif [[ $choice == 3 ]]; then
+if [[ $choice == 1 ]]; then
   echo "Prezto selected!"
   # Copy .termux folder
   cp -fr Termux/ ~/.termux
@@ -73,10 +32,11 @@ elif [[ $choice == 3 ]]; then
     ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
   # Copying over configured files
-  cp -f Prezto/zpreztorc ~/.zpreztorc
-  cp -f Prezto/zshrc ~/.zshrc
+  cp -f Prezto/zpreztorc ~/.zprezto/runcoms/zpreztorc
+  cp -f Prezto/zshrc ~/.zprezto/runcoms/zshrc
   chmod +rwx ~/.zshrc
-  chmod +rw ~/.zsh_history
+  chmod +rw ~/.zhistory
+  chmod +rw ~/.zsh_history_root
   echo "Finished installing Prezto!"
   exit 1;
 else
