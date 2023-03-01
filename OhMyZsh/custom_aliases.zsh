b6a2289ff9b89f58c6d40861f6a50995879b0714
Sohil876 <sohil876@gmail.com>
2023-03-01 17:31:04 +0530

    Add aliases to change font and color scheme

--- OhMyZsh/custom_aliases.zsh
+++ OhMyZsh/custom_aliases.zsh
@@ -2,4 +2,7 @@
 
 # Git
 alias gc='git commit'
+# Termux-zsh
+alias color-changer='bash ${HOME}/.termux/colors.sh'
+alias font-changer='bash ${HOME}/.termux/fonts.sh'
 alias p10k-update='git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull'
