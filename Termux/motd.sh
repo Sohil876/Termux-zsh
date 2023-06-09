799212387647c6ee2721344ec76f8ba46e3ea80e
Sohil876 <sohil876@gmail.com>
2023-06-09 20:30:47 +0530

    Update welcome message

--- Termux/motd.sh
+++ Termux/motd.sh
@@ -1,17 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Welcome message
-echo -e "
-$(toilet -t -f smslant -F crop Termux)
-
-v${TERMUX_VERSION} | ${TERMUX_APK_RELEASE}
-
-Working with packages:
+echo -e "\e[0;32m$(toilet -t -f smslant -F crop Termux)\033[0m\n
+v${TERMUX_VERSION} \e[0;32m|\033[0m ${TERMUX_APK_RELEASE}\n
+\e[0;32mWorking with packages\033[0m:
   Search:  pkg search <query>
   Install: pkg install <package>
   Update:  pkg update
-Subscribing to additional repos:
+\e[0;32mSubscribing to additional repos\033[0m:
   Root:    pkg install root-repo
   X11:     pkg install x11-repo
-For fixing any repository issues:
-  Try \e[4mtermux-change-repo\e[0m command
-"
+\e[0;32mFor fixing any repository issues\033[0m:
+  Try \e[4mtermux-change-repo\e[0m command\n"
